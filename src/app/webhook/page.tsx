8ad3303bbfb452275280644b590b0abcc45d9221
laurammoraesg4 <l.moraes@g4educacao.com>
2025-02-02 20:41:01 -0300

    ajustar

--- src/app/webhook/page.tsx
+++ src/app/webhook/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
-import WebhookView from '../../layouts/WebhookView'
+import WebhookView from '@/layouts/webhookView'
 
 
 
