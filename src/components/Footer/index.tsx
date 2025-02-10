c124e34732d4526e5212fe4fc632618e2cb4cc3c
laurammoraes2@gmail.com <laura@poontoagencia.com>
2025-02-09 22:22:35 -0300

    feat: add landing page

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import style from './style.module.css';
+
+export function Footer(){
+    return(
+        <div className={style.container}>
+            <div className={style.logoWrapper}>
+                <Image
+                    src="/images/logo-copyei.jpeg"
+                    alt="Copyei"
+                    width={150} 
+                    height={50}
+                    priority
+                />
+            </div>
+        </div>
+    )
+}
